87e9d3f6be63049318524ddbf8cf5657ec6548ec
fmitin <fmitin@ivi.ru>
2018-10-22 00:06:55 +0300

    Одна новая выкладка по React и описание в readme.

---
+++ react/comp/State & lifecircle/scripts/reactim.js
@@ -0,0 +1,24 @@
+// Добавление состояния в компонент
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date()
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello state!</h1>
+				<h2>Now is: {this.state.date.toLocaleTimeString()}.</h2>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Clock />,
+	document.getElementById('root')
+);
+
